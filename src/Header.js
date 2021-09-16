98d0e799f5bb1740e9345101d644aca76ccabc78
Hrishikesh Bolinjkar <bolinjkar.rishi@outlook.com>
2021-09-16 12:46:58 +0530

    created header navbar component with styling

---
+++ src/Header.js
@@ -0,0 +1,33 @@
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import './Header.css';
+import HeaderOption from './HeaderOption';
+
+
+const Header = () => {
+    return (  
+        <div className="header">
+            <div className="header__left">
+                <img alt="logo" src="/images/linkedin.svg" />
+                <div className="header__search">
+                    <SearchIcon />
+                    <input type="text"/>
+                </div>
+            </div>
+            <div className="header__right">
+                <HeaderOption Icon={HomeIcon} title="Home"/>
+                <HeaderOption Icon={SupervisorAccountIcon} title="My Network"/>
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
+                <HeaderOption Icon={ChatIcon} title="Messaging"/>
+                <HeaderOption Icon={NotificationsIcon} title="Notifications"/>
+                <HeaderOption avatar="/images/profile_pic.jpeg" title="Me"/>
+            </div>
+        </div>
+    );
+}
+ 
+export default Header;
