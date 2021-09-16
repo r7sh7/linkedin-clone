16bfa1af3d1c903a5e02aaedc43a11577f19d3d2
Hrishikesh Bolinjkar <bolinjkar.rishi@outlook.com>
2021-09-16 16:53:10 +0530

    added Header component to the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+        <Header />
+          {/* App Body */}
+              {/* Sidebar */}
+              {/* Feed */}
+              {/* Widgets */}
     </div>
   );
 }
