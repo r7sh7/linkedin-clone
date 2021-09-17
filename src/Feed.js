5824f485149215ca6ed4807dd5e110f892e852bc
Hrishikesh Bolinjkar <bolinjkar.rishi@outlook.com>
2021-09-17 18:17:02 +0530

    set up firebase and firestore to post a new msg

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CreateIcon from '@material-ui/icons/Create';
 import InsertPhotoIcon from '@material-ui/icons/InsertPhoto';
 import YouTubeIcon from '@material-ui/icons/YouTube';
@@ -8,16 +8,49 @@ import EventIcon from '@material-ui/icons/Event';
 import './Feed.css'
 import InputOption from './InputOption';
 import Post from './Post';
+import firebase from 'firebase';
+import { db } from "./firebase";
 
 function Feed() {
+    const [ posts, setPosts ] = useState([]);
+    const [ input, setInput ] = useState("");
+
+    useEffect(() => {
+        db.collection("posts")
+            .orderBy("timestamp", "desc")
+            .onSnapshot((snapshot) => (
+                setPosts(
+                    snapshot.docs.map((doc) => ({
+                            id: doc.id,
+                            data: doc.data(),
+                        })
+                    )
+                ))
+            );
+    }, []);
+
+
+    const sendPost = (e) => {
+        e.preventDefault();
+
+        db.collection("posts").add({
+            name: 'Megan Fox',
+            description: 'Actor',
+            message: input,
+            photoUrl: 'https://e3.365dm.com/20/06/2048x1152/skynews-megan-fox-actress_5019888.jpg',
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        });
+        setInput("");
+    };
+
     return (
         <div className="feed">
             <div className="feed__inputContainer">
                 <div className="feed__input">
                     <CreateIcon />
                     <form>
-                        <input type="text" />
-                        <button type="submit">submit</button>
+                        <input value={input} onChange={e => setInput(e.target.value)} type="text" />
+                        <button type="submit" onClick={sendPost}>submit</button>
                     </form>
                 </div>
                 <div className="feed__inputOptions">
@@ -28,7 +61,17 @@ function Feed() {
                 </div>
             </div>
             <hr></hr>
-            <Post name="Rishi Bolinjkar" description="Front-End Web Developer" message="This is a test message!"/>
+            {
+                posts.map(({id, data:{name, description, message, photoUrl}}) => (
+                    <Post 
+                        key={id}  
+                        name={name} 
+                        description={description}
+                        message={message}
+                        url={photoUrl}
+                    />
+                ))
+            }
         </div>
     )
 }
