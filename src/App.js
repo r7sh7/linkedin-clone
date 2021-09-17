9206f8a0686d3a731216c87e9cbfb069da71ffb5
Hrishikesh Bolinjkar <bolinjkar.rishi@outlook.com>
2021-09-17 14:29:37 +0530

    added feed to app body

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Feed from './Feed';
 import Header from './Header';
 import SideBar from './SideBar';
 
@@ -9,7 +10,7 @@ function App() {
         <Header />
           <div className="app__body">
               <SideBar />
-              {/* Feed */}
+              <Feed />
               {/* Widgets */}
           </div>
     </div>
