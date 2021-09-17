a5e646983219c9105293a217371c4bef64e1c98f
Hrishikesh Bolinjkar <bolinjkar.rishi@outlook.com>
2021-09-17 18:16:12 +0530

    chore - firebase configuration

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDOX9HAor3yyXyQUy0N2DVno24G3C3aoAU",
+    authDomain: "linkedin-clone-a3cdf.firebaseapp.com",
+    projectId: "linkedin-clone-a3cdf",
+    storageBucket: "linkedin-clone-a3cdf.appspot.com",
+    messagingSenderId: "775826916906",
+    appId: "1:775826916906:web:bbb79ff433910c11f92553"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { db, auth };
