405c5e2190adfb82e036ac0f206463c3abcaca3e
Hrishikesh Bolinjkar <bolinjkar.rishi@outlook.com>
2021-09-22 21:20:38 +0530

    conditionally rendering screen based on authstatus

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
 import Header from './components/Header/Header';
+import Login from './screens/AuthScreen/Login';
 import HomeScreen from './screens/HomeScreen/HomeScreen';
 
 function App() {
+
+  const user = useSelector(state => state.user);
+  
   return (
     <div className="app">
         <Header />
-        <HomeScreen />
+        {user? <HomeScreen /> : <Login />}
     </div>
   );
 }
