4dd06e5761d0df538fe3d338910aff37f509c3d9
Hrishikesh Bolinjkar <bolinjkar.rishi@outlook.com>
2021-09-22 16:32:37 +0530

    feat: added dummy widgets section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Feed from './Feed';
 import Header from './Header';
 import SideBar from './SideBar';
+import Widget from './Widgets';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
           <div className="app__body">
               <SideBar />
               <Feed />
-              {/* Widgets */}
+              <Widget />
           </div>
     </div>
   );
---
+++ src/Widgets.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Widgets.css';
+
+function Widget() {
+    return (
+        <div className="widgets">
+            <div className="widgets__top">
+                <p>LinkedIn News</p>
+                <h2>This is the widgets section</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Widget
