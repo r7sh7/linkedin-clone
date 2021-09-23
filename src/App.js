da7d75183ee52b7a6ffc309fbec45ab800b0472c
Hrishikesh Bolinjkar <bolinjkar.rishi@outlook.com>
2021-09-23 13:50:34 +0530

    useEffect hook to persist login state on refresh

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import './App.css';
 import Header from './components/Header/Header';
+import { auth } from './config/firebase';
 import Login from './screens/AuthScreen/Login';
 import HomeScreen from './screens/HomeScreen/HomeScreen';
+import { LOGIN, LOGOUT } from './store/authConstants';
 
 function App() {
 
   const user = useSelector(state => state.user);
-  
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+      if(userAuth){
+        dispatch({ 
+          type: LOGIN, 
+          payload: {
+            email: userAuth.email,
+            uid: userAuth.uid,
+            name: userAuth.displayName,
+            profilePic: userAuth.photoURL
+        }})
+      }else{
+        dispatch({ type: LOGOUT})
+      }
+    })
+  }, [dispatch])
+
   return (
     <div className="app">
         <Header />
