7460d44cc2e24db87619a0521a1f57456ca7c477
Hrishikesh Bolinjkar <bolinjkar.rishi@outlook.com>
2021-09-27 18:02:31 +0530

    feat: widgets section added

--- src/screens/HomeScreen/components/Widgets/Widgets.js
+++ src/screens/HomeScreen/components/Widgets/Widgets.js
@@ -1,16 +1,39 @@
-import React from 'react';
-import './Widgets.css';
-import InfoIcon from '@mui/icons-material/Info';
+import React from "react";
+import "./Widgets.css";
+import InfoIcon from "@mui/icons-material/Info";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 function Widget() {
-    return (
-        <div className="widgets">
-            <div className="widgets__header">
-                <p>LinkedIn News</p>
-                <InfoIcon />
-            </div>
-        </div>
-    )
+  const newsArticle = (heading, subtitle) => (
+    <div className="widgets__article">
+      <div className="widgets__articleLeft">
+        <FiberManualRecordIcon />
+      </div>
+      <div className="widgets__articleRight">
+        <h4>{heading}</h4>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="widgets">
+      <div className="widgets__header">
+        <p>LinkedIn News</p>
+        <InfoIcon fontSize="small" style={{ fill: "grey" }} />
+      </div>
+      {newsArticle("The 25 startups to watch", "Top news - 36,283 readers")}
+      {newsArticle("What not to say in an email", "Top news - 36,283 readers")}
+      {newsArticle("The 25 startups to watch", "Top news - 36,283 readers")}
+      {newsArticle("The 25 startups to watch", "Top news - 36,283 readers")}
+      {newsArticle("The 25 startups to watch", "Top news - 36,283 readers")}
+      <div className="widgets__bottom">
+        <h4>Show more</h4>
+        <ExpandMoreIcon />
+      </div>
+    </div>
+  );
 }
 
-export default Widget
+export default Widget;
