6b81bda23f0f6f7e55ae3bfbebdd38401ba9fb79
Hrishikesh Bolinjkar <bolinjkar.rishi@outlook.com>
2021-10-02 19:58:53 +0530

    feat: ability to add video and images to posts

--- src/store/authActions.js
+++ src/store/authActions.js
@@ -1,5 +1,5 @@
 import { LOGIN, LOGOUT } from "./authConstants";
-import { auth, provider } from "../config/firebase";
+import { auth, db, provider, storage } from "../config/firebase";
 
 export const login = (userAuth) => {
   return (dispatch) => {
@@ -42,5 +42,63 @@ export function getUserAuth() {
         dispatch(logout());
       }
     });
+  };
+}
+
+export function createPostApi(post) {
+  return (dispatch) => {
+    if (post.image !== "") {
+      const upload = storage.ref(`images/${post.image.name}`).put(post.image);
+      upload.on(
+        "state_changes",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Progress: ${progress}`);
+          if (snapshot.state === "RUNNING") {
+            console.log(`Progress: ${progress}`);
+          }
+        },
+        (error) => console.log(error.code),
+        async () => {
+          const downloadURL = await upload.snapshot.ref.getDownloadURL();
+          db.collection("posts").add({
+            user: {
+              name: post.user.displayName,
+              email: post.user.email,
+              photoURL: post.user.photoURL,
+            },
+            video: post.video,
+            sharedImage: downloadURL,
+            description: post.description,
+            timestamp: post.timestamp,
+          });
+        }
+      );
+    } else if (post.video) {
+      db.collection("posts").add({
+        user: {
+          name: post.user.displayName,
+          email: post.user.email,
+          photoURL: post.user.photoURL,
+        },
+        video: post.video,
+        sharedImage: "",
+        description: post.description,
+        timestamp: post.timestamp,
+      });
+    } else {
+      db.collection("posts").add({
+        user: {
+          name: post.user.displayName,
+          email: post.user.email,
+          photoURL: post.user.photoURL,
+        },
+        video: "",
+        sharedImage: "",
+        description: post.description,
+        timestamp: post.timestamp,
+      });
+    }
   };
 }
